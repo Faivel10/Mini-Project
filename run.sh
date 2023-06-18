c16f66cb7048a6f61ba14578b73db61eeac34c55
faivel dragatsky <faivel.dragatsky@morphisec.com>
2023-06-18 22:43:10 +0300

    working output to csv

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ python3 ./reader.py
 g++ ./main.cpp -o ./main
 
 ./main
+
+python3 ./writer.py
--- writer.py
+++ writer.py
@@ -9,9 +9,10 @@ for file in os.listdir("./output"):
         with open(file_path, 'r') as file:
             lines = file.readlines()        
             # Create a CSV file for writing
-            file_new_loc = "./output-csv/" + file.name
+            file_new_loc = "./output-csv/" + file.name.split("/")[2] + ".csv"
             with open(str(file_new_loc), 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile)
                 for line in lines:
                     x, y = line.strip().split(' ')
                     writer.writerow([x, y])
+                
